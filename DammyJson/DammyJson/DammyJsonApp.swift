b5e71f5b9fce0a3739c943fe0960a16311c4ec81
Joonatan De Pascale <joonatan.depascale@tuni.fi>
2023-05-16 10:55:35 +0300

    :tada: Create iOS project

---
+++ DammyJson/DammyJson/DammyJsonApp.swift
@@ -0,0 +1,17 @@
+//
+//  DammyJsonApp.swift
+//  DammyJson
+//
+//  Created by Joonatan De Pascale on 16.5.2023.
+//
+
+import SwiftUI
+
+@main
+struct DammyJsonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
