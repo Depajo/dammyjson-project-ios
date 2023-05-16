c2d84026871d8bf36a11bb6935829bc312b6aeec
Joonatan De Pascale <joonatan.depascale@tuni.fi>
2023-05-16 12:00:05 +0300

    :sparkles: Added getData function in FetchTools file

---
+++ DammyJson/DammyJson/FetchTools.swift
@@ -0,0 +1,24 @@
+//
+//  FetchTools.swift
+//  DammyJson
+//
+//  Created by Joonatan De Pascale on 16.5.2023.
+//
+
+import Foundation
+import Alamofire
+
+//Get data
+func getData(url: URLComponents, dataCallback: @escaping (Users)->()) {
+    AF.request(url).responseDecodable(of: Users.self) { response in
+        switch response.result {
+            case .success(let responseData):
+            dataCallback(responseData)
+            
+            case .failure(let error):
+                print(error)
+            }
+    }
+}
+
+
