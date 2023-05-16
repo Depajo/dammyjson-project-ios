c914f9ed0b23444f76c4dbf6ea11b0ebb68b4cc6
Joonatan De Pascale <joonatan.depascale@tuni.fi>
2023-05-16 17:24:47 +0300

    :sparkles: SearchBar is added on ContentView

--- DammyJson/DammyJson/ContentView.swift
+++ DammyJson/DammyJson/ContentView.swift
@@ -8,18 +8,34 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var users: Users?
+    @State var searchText: String = ""
+    
     var body: some View {
         VStack {
-            UserList()
+            SearchBar(users: $users, searchText: $searchText, placeholder: "Hae")
+            UserListIsEmpty(users: $users)
+            UserList(users: $users)
+        }
+    }
+}
+
+struct UserListIsEmpty: View {
+    @Binding var users: Users?
+    var body: some View {
+        if let usersData = users {
+            if usersData.users.isEmpty {
+                Text("User data not found")
+                    .bold()
+            }
         }
     }
 }
 
 struct UserList: View {
-    @State var users: Users?
     var fetch = FetchTools()
+    @Binding var users: Users?
     var body: some View {
-        
         VStack {
             List {
                 if let usersData: Users = users {
@@ -28,19 +44,47 @@ struct UserList: View {
                             .padding(0)
                         
                     }
-                    
                 } else {
                     ProgressView()
                 }
             }
             .padding(0)
             .onAppear() {
-                fetch.getData(url: URLComponents(string: "https://dummyjson.com/users?limit=0")!) { data in
-                    users = data
-                    print(data)
+                if let url = URLComponents(string: "https://dummyjson.com/users?limit=0") {
+                    fetch.getData(url: url) { data in
+                        users = data
+                        print(data)
+                    }
                 }
             }
         }
+    }
+}
+
+struct SearchBar: View {
+    var fetch = FetchTools()
+    @Binding var users: Users?
+    @Binding var searchText: String
+    var placeholder: String
+    var body: some View {
+        TextField(placeholder, text: $searchText)
+            .onChange(of: searchText) { newText in
+                let text = searchText.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!
+                print(text)
+                if let url = URLComponents(string: "https://dummyjson.com/users/search?q=\(text)") {
+                    fetch.getData(url: url) { data in
+                        users = data
+                        print(data)
+                    }
+                }
+            }
+            .padding(10)
+            .font(.system(size: 20))
+            .frame(alignment: .center)
+            .background(RoundedRectangle(cornerRadius: 20) .stroke(Color.gray))
+            .padding(10)
+        
+        
     }
 }
 
